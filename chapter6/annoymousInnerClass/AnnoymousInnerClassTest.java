60db5ce431d0516806052d5066422f75ef4aaf25
yfjiang <winter0130jyf@gmail.com>
2020-04-24 22:30:56 +0800

    chapter6

---
+++ chapter6/annoymousInnerClass/AnnoymousInnerClassTest.java
@@ -0,0 +1,48 @@
+package annoymousInnerClass;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+/**
+ * This program demonstrates annoymous inner classes.
+ * 
+ * @version 6.6.1 2020-4-24
+ * @author yefan_jiang
+ */
+public class AnnoymousInnerClassTest {
+    public static void main(String[] args) {
+        TalkingClock clock = new TalkingClock();
+        clock.start(1000, true);
+        
+        // keep program running until user selects "OK"
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
+
+/**
+ * A clock that prints the time in regular intervals
+ */
+class TalkingClock {
+    /**
+     * Starts the clock
+     * @param interval the interval between messages (in milliseconds)
+     * @param beep true if the clock should beep
+     */
+    public void start(int interval, boolean beep)
+    {
+        ActionListener listener = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                System.out.println("At the tone, the time is " + new Date());
+                if (beep) Toolkit.getDefaultToolkit().beep();
+            }
+        };
+        Timer t = new Timer(interval, listener);
+        t.start();
+    }
+}
